95c73bd62ac258e8f6152b87959365f305d1449a
kazukousen <kzkntt@stu.kanazawa-u.ac.jp>
2016-03-25 22:48:21 +0900

    とりあえずタイトルとURLを表示

--- nokogiri-parse.rb
+++ nokogiri-parse.rb
@@ -6,4 +6,10 @@ opt = {}
 opt['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36 Vivaldi/1.0.403.24'
 
 doc = Nokogiri::HTML(open(url, opt))
-p doc.class
+nodesets = doc.xpath('//h3/a')
+
+nodesets.each do |nodeset|
+  puts nodeset.text
+  puts nodeset.[]('href')
+  puts "\n"
+end
