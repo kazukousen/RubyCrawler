57beafaa5b66aa9bda92c587ef2669cc96c7de31
kazukousen <kzkntt@stu.kanazawa-u.ac.jp>
2016-03-25 22:40:01 +0900

    Nokogiri初回

---
+++ nokogiri-parse.rb
@@ -0,0 +1,9 @@
+require 'nokogiri'
+require 'open-uri'
+
+url = 'http://b.hatena.ne.jp/i101330/bookmark'
+opt = {}
+opt['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36 Vivaldi/1.0.403.24'
+
+doc = Nokogiri::HTML(open(url, opt))
+p doc.class
